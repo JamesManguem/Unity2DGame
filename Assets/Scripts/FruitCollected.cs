dc794d5f8e5037168edcc7f2dd238142e1bd4061
JamesManguem <18090067@alumno.utmetropolitana.edu.mx>
2021-10-24 20:18:46 -0500

    Segundocommitanimacionnivel

--- Assets/Scripts/FruitCollected.cs
+++ Assets/Scripts/FruitCollected.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FruitCollected : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
@@ -11,9 +12,6 @@ public class FruitCollected : MonoBehaviour
             GetComponent<SpriteRenderer>().enabled = false;
 
             gameObject.transform.GetChild(0).gameObject.SetActive(true);
-
-
-            FindObjectOfType<FruitManager>().AllFruitsCollected();
 
 
             Destroy(gameObject, 0.5f);
